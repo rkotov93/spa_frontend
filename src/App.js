d3747a210cebf12031cf98aac8ecd240388e987a
Ruslan Kotov <rkotov93@gmail.com>
2016-11-18 02:37:34 +0200

    Add some testing data. Add Post props validation

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import PostForm from "./PostForm"
 
 export default class App extends Component {
   render() {
+    const news = [
+      {id: 1, name: "News1", description: "Description of News1"},
+      {id: 2, name: "News2", description: "Description of News2"},
+      {id: 3, name: "News3", description: "Description of News3"}
+    ]
     return (
       <div>
-        <Post />
+        {
+          news.map(post => {
+            return <Post key={`post_${post.id}`} {...post} />
+          })
+        }
         <PostForm />
       </div>
     )
--- src/Post.js
+++ src/Post.js
@@ -3,7 +3,16 @@ import React, {Component} from "react"
 export default class Post extends Component {
   render() {
     return (
-      <h1>Super news!</h1>
+      <div id={this.props.id}>
+        <h1>{this.props.name}</h1>
+        <p>{this.props.description}</p>
+      </div>
     )
   }
+}
+
+Post.propTypes = {
+  id: React.PropTypes.number.isRequired,
+  name: React.PropTypes.string.isRequired,
+  description: React.PropTypes.string
 }
