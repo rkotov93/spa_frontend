531484de24635d73720852bfbac428729190106d
Ruslan Kotov <rkotov93@gmail.com>
2016-11-18 13:03:50 +0200

    Fix eslint config for curly braces

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import Post from "./Post"
 import PostForm from "./PostForm"
 
 export default class App extends Component {
   render() {
     const news = [
-      {id: 1, name: "News1", description: "Description of News1"},
-      {id: 2, name: "News2", description: "Description of News2"},
-      {id: 3, name: "News3", description: "Description of News3"}
+      { id: 1, name: "News1", description: "Description of News1" },
+      { id: 2, name: "News2", description: "Description of News2" },
+      { id: 3, name: "News3", description: "Description of News3" }
     ]
     return (
       <div>
