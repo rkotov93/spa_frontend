fccc127775f0fe086174dd7711f93091c9dfbb1d
Ruslan Kotov <rkotov93@gmail.com>
2016-12-02 02:37:18 +0200

    Replace input components into unified PostForm component

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react"
+import ReactDOM from "react-dom"
 
-import { Row, Col, Button } from "react-bootstrap"
-
-import PostTitleInput from "./PostFormInputs/PostTitleInput"
-import PostBodyInput from "./PostFormInputs/PostBodyInput"
+import { Row, Col, Button, FormGroup, FormControl } from "react-bootstrap"
 
 export default class PostForm extends Component {
   constructor() {
     super()
     this.state = {
+      values: {
+        title: "",
+        body: ""
+      },
       validity: {
+        title: false,
+        body: false
+      },
+      touched: {
         title: false,
         body: false
       }
@@ -23,8 +29,26 @@ export default class PostForm extends Component {
           <h3>Add your post</h3>
 
           <form ref="newPostForm">
-            <PostTitleInput ref="titleInput" setValidity={this._setValidity.bind(this)} />
-            <PostBodyInput ref="bodyInput" setValidity={this._setValidity.bind(this)} />
+            <FormGroup validationState={this._titleInputValidationState()}>
+              <FormControl
+                ref="titleInput"
+                type="text"
+                placeholder="Post title"
+                name="post[title]"
+                onChange={this._handleInputChange.bind(this)}
+                data-name="title"
+              />
+            </FormGroup>
+            <FormGroup validationState={this._bodyInputValidationState()}>
+              <FormControl
+                ref="bodyInput"
+                componentClass="textarea"
+                placeholder="Content"
+                name="post[body]"
+                onChange={this._handleInputChange.bind(this)}
+                data-name="body"
+              />
+            </FormGroup>
             <Button
               bsStyle="primary"
               style={{ width: "100%" }}
@@ -39,14 +63,76 @@ export default class PostForm extends Component {
     )
   }
 
-  _setValidity(input, status) {
-    const validity = this.state.validity
-    validity[input] = status
-    this.setState({ validity: validity })
-  }
-
   _isFormValid() {
     return this.state.validity.title && this.state.validity.body
+  }
+
+  _handleInputChange(e) {
+    const field = e.target.dataset["name"]
+    const value = e.target.value
+    const validity = this._validate(field, value)
+
+    const valuesState = this.state.values
+    valuesState[field] = value
+
+    const validityState = this.state.validity
+    validityState[field] = validity
+
+    const touchedState = this.state.touched
+    touchedState[field] = true
+
+    this.setState({ values: valuesState, validity: validityState, touched: touchedState })
+  }
+
+  _validate(field, value) {
+    if (field === "title")
+      return this._isTitleValid(value)
+    else if (field === "body")
+      return this._isBodyValid(value)
+    return false
+  }
+
+  _isTitleValid(value) {
+    return value.length > 0 && value.length <= 120
+  }
+
+  _isBodyValid(value) {
+    return value.length > 0
+  }
+
+  _titleInputValidationState() {
+    const touched = this.state.touched.title
+    const valid = this.state.validity.title
+
+    if (valid) return "success"
+    else if (touched && !valid) return "error"
+  }
+
+  _bodyInputValidationState() {
+    const touched = this.state.touched.body
+    const valid = this.state.validity.body
+
+    if (valid) return "success"
+    else if (touched) return "error"
+  }
+
+  _clearInputs() {
+    ReactDOM.findDOMNode(this.refs.titleInput).value = ""
+    ReactDOM.findDOMNode(this.refs.bodyInput).value = ""
+    this.setState({
+      values: {
+        title: "",
+        body: ""
+      },
+      validity: {
+        title: false,
+        body: false
+      },
+      touched: {
+        title: false,
+        body: false
+      }
+    })
   }
 
   _handleSubmit() {
@@ -54,8 +140,7 @@ export default class PostForm extends Component {
       method: "POST",
       body: new FormData(this.refs.newPostForm)
     }).then(response => {
-      this.refs.titleInput._clearInput()
-      this.refs.bodyInput._clearInput()
+      this._clearInputs()
       return response.json()
     }).then(newPost => {
       this.props.addNewPost(newPost)
