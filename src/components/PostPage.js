db74d48a35c4a6abdaca079b2ac36288ffd9acc2
Ruslan Kotov <rkotov93@gmail.com>
2017-01-19 16:18:01 +0200

    [PostPage] Show avatar

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Row, Col, Button } from 'react-bootstrap'
 
-const PostPage = ({ id, title, body, author, onDestroy, isAuthenticated }) => {
+const PostPage = ({ id, title, body, author, avatar, onDestroy, isAuthenticated }) => {
   return (
     <Row id={id} className='news-post'>
       <Col md={12}>
@@ -16,6 +16,7 @@ const PostPage = ({ id, title, body, author, onDestroy, isAuthenticated }) => {
         <h3 className='post-title'>{title}</h3>
         <p className='post-body'>{body}</p>
         <p className='post-author'>{author}</p>
+        {avatar && <img src={avatar} />}
         <hr />
       </Col>
     </Row>
